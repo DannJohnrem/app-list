747c04328f9b7baa908b2119dc6334524e9bdb3d
Dann Johnrem De Guzman <jonrhem10@gmail.com>
2022-10-26 13:31:37 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
-import Footer from '../components/footer';
+// import Footer from '../components/footer';
 import Navbar from '../components/Navbar';
 import styles from '../styles/Home.module.css'
 
@@ -13,7 +13,7 @@ export default function Home() {
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae vel, impedit accusamus animi saepe labore esse consequuntur sit officia quisquam magni fugit repellat molestias cupiditate repudiandae fuga nam id ea.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae vel, impedit accusamus animi saepe labore esse consequuntur sit officia quisquam magni fugit repellat molestias cupiditate repudiandae fuga nam id ea.</p>
       <Link href="/items">See Item list</Link>
-      <Footer/>
+      {/* <Footer/> */}
     </div> 
   );
 }
