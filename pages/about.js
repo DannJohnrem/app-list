e7bdc01645bf8dcb021a9f431c09f20771fd46cc
Dann Johnrem De Guzman <jonrhem10@gmail.com>
2022-10-26 13:28:22 +0800

    first commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+const about = () => {
+    return ( 
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, impedit id? Esse nulla aut rerum dolorem dolor assumenda, fugit maxime magnam eaque. Sint eius qui harum velit est cum error.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, impedit id? Esse nulla aut rerum dolorem dolor assumenda, fugit maxime magnam eaque. Sint eius qui harum velit est cum error.</p>
+
+        </div>
+    );
+}
+ 
+export default about;
---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+import Footer from '../components/footer';
+import Navbar from '../components/Navbar';
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar/>
+      <h1>Homepage</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae vel, impedit accusamus animi saepe labore esse consequuntur sit officia quisquam magni fugit repellat molestias cupiditate repudiandae fuga nam id ea.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae vel, impedit accusamus animi saepe labore esse consequuntur sit officia quisquam magni fugit repellat molestias cupiditate repudiandae fuga nam id ea.</p>
+      <Link href="/items">See Item list</Link>
+      <Footer/>
+    </div> 
+  );
+}
